b34218f77f4bcbcc3ad76388d05ad10bdeba589b
kwonho <76275193+AiBiC-Data@users.noreply.github.com>
2024-01-15 10:46:19 +0900

    Docs: rectify comments

--- install.sh
+++ install.sh
@@ -2,40 +2,46 @@
 
 # tested on ubuntu 22.04 (LTS)
 
-# 도커 설치
+# Install Docker
 
-# 1. 우분투 시스템 패키지 업데이트
+# 1.Update Ubuntu Systems
 sudo apt-get update
-# 2. 필요한 패키지 설치
+
+# 2. Install packages
 sudo apt-get install -y\
     apt-transport-https \
     ca-certificates\
     curl\
     gnupg-agent\
     software-properties-common
-# 3. Docker의 공식 GPG키를 추가
+    
+# 3. Add Docker License GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# 4. Docker의 공식 apt 저장소를 추가
+
+# 4. Docker의 공식 apt 저장소를 추가 Add docker License apt repository
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# 5. 시스템 패키지 업데이트
+
+# 5. Update system package
 sudo apt-get update
 sudo apt-get install net-tools
-# 6. Docker 설치
+
+# 6. Install Docker
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin
-# 7. Docker 실행
+
+# 7. Start Docker
 systemctl enable docker
 systemctl start docker
 #systemctl status docker
 
-# 도커에 젠킨스 올리기
+# Start Jenkins with Docker
 
-# 젠킨스 저장소 설치
+# Make jenkins folder
 mkdir -p /var/jenkins_home
-#  권한 설정
 chown -R 1000:1000 /var/jenkins_home/
-# 호스트9000:컨테이너8080 매핑/ 포트50000을 통신위해 매핑
+
+# Mapping Container and Host 
 docker run --restart=on-failure --user='root' -p 9000:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -d --name jenkins jenkins/jenkins:lts
 
-# show endpoint
+# Show endpoint
 echo 'Jenkins installed'
 echo 'access jenkins at: http://'$(curl -4 -s ifconfig.co)':9000'
