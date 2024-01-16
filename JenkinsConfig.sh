26255318259551a9121ad49f80ce4b4360f2afa8
kwonho <76275193+AiBiC-Data@users.noreply.github.com>
2024-01-16 17:03:01 +0900

    Fix: Change debian to ubuntu

--- JenkinsConfig.sh
+++ JenkinsConfig.sh
@@ -10,7 +10,7 @@ sudo docker exec --user='root' -it jenkins apt-get -y install apt-transport-http
          curl \
          gnupg-agent \
          software-properties-common && \
-sudo docker exec --user='root' -it jenkins curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - && \
+sudo docker exec --user='root' -it jenkins curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo docker exec --user='root' -it jenkins apt-get update && \
 apt-get install -y docker-ce docker-ce-cli containerd.io
