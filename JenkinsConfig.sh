6f17d923e5c19ea5c9e3137652481d25551cd97a
kwonho <76275193+AiBiC-Data@users.noreply.github.com>
2024-01-28 15:43:48 +0900

    Docs: Update Conf wt. Debian

--- JenkinsConfig.sh
+++ JenkinsConfig.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# jenkins 컨테이너 root 패스워드 뭐지??
+apt-get update
+apt-get install openjdk-17-jdk -y
 
-sudo docker exec --user='root' -it jenkins apt-get update
-sudo docker exec --user='root' -it jenkins apt-get install openjdk-17-jdk -y
+apt-get install -y \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg2 \
+software-properties-common
 
-sudo docker exec --user='root' -it jenkins apt-get -y install apt-transport-https \
-         ca-certificates \
-         curl \
-         gnupg-agent \
-         software-properties-common && \
-sudo docker exec --user='root' -it jenkins curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo docker exec --user='root' -it jenkins apt-get update && \
-apt-get install -y docker-ce docker-ce-cli containerd.io
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 
-sudo docker exec -it jenkins bash
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
-echo $JAVA_HOME
+add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/debian \
+$(lsb_release -cs) \
+stable"
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
